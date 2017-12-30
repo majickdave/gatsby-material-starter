1ce660d955859e3d3b0fbf804317584e55122892
David Samuel <cryptondave@gmail.com>
2017-12-29 23:08:08 -0800

    added discuss

---
+++ content/sample-posts/29-12-2017/index.md
@@ -0,0 +1,18 @@
+---
+title: "majicDave's first blog post"
+cover: "https://picsum.photos/200/300/?random"
+date: "29/12/2017"
+category: "tech"
+tags:
+- programming
+- more tags
+- testing
+- another one
+- stuff
+- other
+---
+# This is my first blog post
+
+## Check it out
+
+Hello, World! 🌎
---
+++ data/SiteConfig.js
@@ -0,0 +1,40 @@
+module.exports = {
+  blogPostDir: "sample-posts", // The name of directory that contains your posts.
+  siteTitle: "David Samuel's Blog", // Site title.
+  siteTitleAlt: "A blog site created with GatsbyJS by David Samuel", // Alternative site title for SEO.
+  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteUrl: "https://www.davidsamuel.me", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
+  siteDescription: "A blog about tech, music, and stuff.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteFBAppID: "170043350414321", // FB Application ID for using app insights
+  siteGATrackingID: "UA-111710660-1", // Tracking code ID for google analytics.
+  disqusShortname: "davidsamuel", // Disqus shortname.
+  postDefaultCategoryID: "Tech", // Default category for posts.
+  userName: "majickdave", // Username to display in the author segment.
+  userTwitter: "majickdave", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Los Angeles, California, USA", // User location to display in the author segment.
+  userAvatar: "https://www.dropbox.com/s/u7egnn54iznzp61/me_avatar_small.png?raw=1", // User avatar to display in the author segment.
+  userDescription:
+    "Data Scientist, and technology dork", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/majickdave",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/majickdave",
+      iconClassName: "fa fa-twitter"
+    },
+    {
+      label: "Linkedin",
+      url: "https://linkedin.com/in/datascienceinsight",
+      iconClassName: "fa fa-linkedin"
+    }
+  ],
+  copyright: "Copyright © 2017. David Samuel" // Copyright string for the footer of the website and RSS feed.
+};
